9af6bffbdf12d908fc23488223bb672dc55dce00
liannenn <hiraethals@gmail.com>
2022-08-15 14:16:35 -0500

    Update test program.py

--- Chapter 2/test program.py
+++ Chapter 2/test program.py
@@ -3,3 +3,10 @@
 def print_something():
     #print a message
     print("This is your message")
+    
+#another function recieves no arguments
+#it just output two messages separated by a blank line
+def another_function():
+    print("This is message 1")
+    print()
+    print("This is message 2")
