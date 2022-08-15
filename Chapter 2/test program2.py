edc79e5332c0d4a6c5c7fb774ec8e3465c5051fc
liannenn <hiraethals@gmail.com>
2022-08-15 14:36:49 -0500

    Update test program2.py

--- Chapter 2/test program2.py
+++ Chapter 2/test program2.py
@@ -16,3 +16,8 @@ def program2_2(): #double quote output
     print ("Kate Austen")
     print("123 Full Circle Drive")
     print ("Asheville, NC 28899")
+    
+def program2_3():
+    #program 2 3
+    print("Don't fear!")
+    print("I'm here!")
