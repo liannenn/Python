d61fa572af4d9acb83ec5d21157673d616d8892a
liannenn <hiraethals@gmail.com>
2022-08-15 14:33:13 -0500

    Create test program2.py

---
+++ Chapter 2/test program2.py
@@ -0,0 +1,18 @@
+def comment_example():
+    #comment_example receives no arguments
+    #it explains how to create a function header
+    #and outputs or returns "Hello world!".
+    print('Hello world!')
+    
+def program2_1():
+    #program 2 1 receives no arguments
+    #it outputs three messages using apostrophes instead
+    #of quotation marks
+    print('Kate Austen')
+    print('123 Full Circle Drive')
+    print('Asheville, NC 28899')
+    
+def program2_2(): #double quote output
+    print ("Kate Austen")
+    print("123 Full Circle Drive")
+    print ("Asheville, NC 28899")
