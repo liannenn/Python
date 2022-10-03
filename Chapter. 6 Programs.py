947c90cdb4b3fbd74e3c264fd63bc18998413192
liannenn <hiraethals@gmail.com>
2022-10-03 14:44:49 -0500

    e

---
+++ Chapter. 6 Programs.py
@@ -0,0 +1,27 @@
+def line_numbers():
+    
+    try:
+        file = open("steps.txt", "r")
+        
+    except:
+        print("[Errno 2] No such file or directory: '",file,"'")    
+
+def line_counter():
+    
+    try:
+    
+        file = input("Enter the file to read: ")
+        openfile = open(file, 'r')
+        line = 
+        
+        print(file, "contains", line, "lines.")
+        
+    except:
+        print("[Errno 2] No such file or directory: '",file,"'")
+        
+def average_of_numbers():
+    
+    try:
+        
+        
+        
