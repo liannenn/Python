e0a8c791be6b94725a6de18c69a203b16d5145af
Jullian Tran <2017696@student.goddardusd.com>
2022-11-28 14:43:53 -0600

    Update Chapter 9 Programs.py

--- Chapter 2/Chapter 9 Programs.py
+++ Chapter 2/Chapter 9 Programs.py
@@ -4,7 +4,7 @@ def file_encryption():
     super_l33t_codes = {'A' : 'Q', 'S' : 'W', 'D' : 'E', 'F' : 'E', 'G' : 'T', 'H' : 'Y', 'J' : 'U', 'K' : 'I',
                         'L' : 'O','Q' : '1', 'W' : '2', 'E' : '3', 'R' : '4', 'T' : '5', 'Y' : '6', 'U': '7',
                         'I' : '8', 'O' : '9', 'P' : '0', 'Z' : 'A' , 'X' : 'S', 'C' : 'D', 'V' : 'F', 'B' : 'G',
-                        'N' : 'H', 'M' : 'J', ',' : ':', '.' : ' " '}
+                        'N' : 'H', 'M' : 'J', ',' : ':', '.' : ' " ', " " : " ", "\n" : ""}
     file = input("Input a file to open: ")
     
     infile = open(file, 'r')
@@ -12,18 +12,25 @@ def file_encryption():
     line = ''
     
     newmessage = ''
+
     
     for line in infile:
-        print(line)
         #split line into a list
         #process through the list similar to below
         for letter in line:
             letterupper = letter.upper()
             newmessage += super_l33t_codes[letterupper]
-            print(super_l33t_codes[letterupper])
+           
             #counter +=1
             
-        if 
-    print("Your encrypted file is", newmessage)
-            
+    print("Your encrypted file is:", newmessage)
+    
+def file_decryption():
+    
+     super_l33t_codes = {'Q' : 'A', 'W' : 'S', 'E' : 'D', 'E' : 'F', 'T' : 'G', 'Y' : 'H', 'U' : 'J', 'I' : 'K',
+                        'O' : 'L', '1' : 'Q', '2' : 'W', '3' : 'E', '4' : 'R', '5' : 'T', '6' : 'Y', 'U': '7',
+                        'I' : '8', 'O' : '9', 'P' : '0', 'Z' : 'A' , 'X' : 'S', 'C' : 'D', 'V' : 'F', 'B' : 'G',
+                        'N' : 'H', 'M' : 'J', ',' : ':', '.' : ' " ', " " : " ", "\n" : ""}
+    
+    
     
